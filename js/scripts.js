578232fa03c99c88ade433917f709c94f1870643
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2021-11-30 20:30:38 -0800

    Add Address Book

---
+++ js/scripts.js
@@ -0,0 +1,117 @@
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
+
+// Business Logic for Contacts ---------
+function EmailTypes(personalEmail, workEmail, spamEmail) {
+  this.personalEmail = personalEmail;
+  this.workEmail = workEmail;
+  this.spamEmail = spamEmail;
+}
+
+// function emailAddress(personalEmail, workEmail, spamEmail) {
+//   this.personalEmail = personalEmail;
+//   this.workEmail = workEmail;
+//   this.spamEmail = spamEmail;
+// }
+
+function Contact(firstName, lastName, phoneNumber, emailTypes, physicalAddress) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+  this.emailTypes = {};
+  this.physicaladdress = physicalAddress;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
+
+// User Interface Logic ---------
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+}
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  $(".email-address").html(contact.emailAddress(inputtedPersonalEmail, inputtedWorkEmail, inputtedSpamEmail)); // testing email types
+  $(".physical-address").html(contact.physicalAddress);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function() {
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+}
+
+$(document).ready(function() {
+  attachContactListeners();
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    const inputtedEmailAddress = $("input#new-email-address").val(); // testing email types
+    const inputtedPersonalEmail = $("input#new-personal-email").val(); // testing email types
+    const inputtedWorkEmail = $("input#new-work-email").val(); // testing email types
+    const inputtedSpamEmail = $("input#new-spam-email").val(); // testing email types
+    const inputtedPhysicalAddress = $("input#new-physical-address").val();
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+    $("input#new-email-address").val("");
+    $("input#new-physical-address").val("");
+
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, inputtedPhysicalAddress);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  });
+});
